fef1f16ba71a877baf9e446a5449e974e15402de
Akapo Damilola Francis <fdamilola@gmail.com>
2011-08-29 13:51:09 -0400

    added new method for mutiplying arrays

--- eul_me.rb
+++ eul_me.rb
@@ -1,9 +1,15 @@
-def array_sum (value)
+def array_sum(value)
 value.inject(0) {|sum, element| sum += element}
 end
 
 def conv_to_array(value)
 value.to_a
 end
-$start = Time.now
-$time_end = Time.now - $start
+
+def chng_str_to_array(value)
+value.split(//)
+end
+
+def array_mul(value)
+value.inject(1) {|mul, element| mul *= element}
+end
