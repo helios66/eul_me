f7be90e871abceb77895f2c11c2a16dd53ac7ce2
Akapo Damilola Francis <fdamilola@gmail.com>
2011-10-16 17:42:00 +0100

    added more methods to the eul_me.rb file

--- eul_me.rb
+++ eul_me.rb
@@ -37,3 +37,46 @@ x2 = (-b.to_f - (nroot(_test.to_f, 2)))/(2*a.to_f)
 puts "x1 : #{x1} \n"
 puts "x2 : #{x2} \n"
 end
+
+def combination(n, r)
+#defining and calculating the combination of two given #numbers
+(factorial(n))/(factorial(r)*factorial(n-r))
+end
+
+def permutation(n, r)
+#calculates the permutation of two given numbers
+factorial(n)/factorial(r)
+end
+
+def fact(n) #factorial in another light
+  if n == 0
+    1
+  else
+    n * fact(n-1)
+  end
+end
+
+def mod(num)
+if num < 0.0
+md = -1 * num
+elsif num > 0.0
+md = 1 * num
+elsif num == 0 or num == 0.0
+md = 0
+end
+end
+
+def isprime(n)
+$num = []
+for i in (1..n).to_a do
+s = (n.to_f)%(i.to_f)
+if s == 0.0
+$num << i
+end
+end
+if $num.size == 2 && $num == [1, n]
+return true
+else
+return false
+end
+end
